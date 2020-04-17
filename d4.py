c4273ede96868f27b2debec89fca4cf0e55b6bbb
alexcnr <54246135+alexcnr@users.noreply.github.com>
2020-04-17 20:21:50 +0400

    Add files via upload
    Мигание световодом в цикле и выход

---
+++ d4.py
@@ -0,0 +1,19 @@
+#код работает
+import RPi.GPIO as GPIO 
+import time
+
+#GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+
+
+
+for i in range(10): 
+    GPIO.output(12,True)
+    time.sleep(0.5)
+    GPIO.output(12,False)
+    time.sleep(0.5)
+    
+GPIO.cleanup()
+print("Мигания закончились!")
